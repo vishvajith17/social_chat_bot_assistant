3ebc008e0f48a64df4d180701657ac0dbc641a6b
vishvajith17 <maduka.18@cse.mrt.ac.lk>
2021-09-07 17:32:08 +0530

    train module for lost_something feature

--- lib/screens/TransportDomain/lost_something_screen.dart
+++ lib/screens/TransportDomain/lost_something_screen.dart
@@ -1,31 +1,19 @@
 import 'package:bubble/bubble.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dialogflow/dialogflow_v2.dart';
-import 'package:intl/intl.dart';
 import 'package:social_chat_bot_assistant/constants.dart';
+import 'package:intl/intl.dart';
 
 class LostSomething extends StatefulWidget {
-  LostSomething({Key key, this.title}) : super(key: key);
-
-// This widget is the home page of your application. It is stateful, meaning
-// that it has a State object (defined below) that contains fields that affect
-// how it looks.
-
-// This class is the configuration for the state. It holds the values (in this
-// case the title) provided by the parent (in this case the App widget) and
-// used by the build method of the State. Fields in a Widget subclass are
-// always marked "final".
-
-  final String title;
-
   @override
   _LostSomethingState createState() => _LostSomethingState();
 }
 
 class _LostSomethingState extends State<LostSomething> {
   void response(query) async {
-    AuthGoogle authGoogle =
-        await AuthGoogle(fileJson: "assets/service.json").build();
+    AuthGoogle authGoogle = await AuthGoogle(
+            fileJson: "assets/lost-something-plgg-678667f69a6c.json")
+        .build();
     Dialogflow dialogflow =
         Dialogflow(authGoogle: authGoogle, language: Language.english);
     AIResponse aiResponse = await dialogflow.detectIntent(query);
@@ -35,8 +23,7 @@ class _LostSomethingState extends State<LostSomething> {
         "message": aiResponse.getListMessage()[0]["text"]["text"][0].toString()
       });
     });
-
-    print(aiResponse.getListMessage()[0]["text"]["text"][0].toString());
+    print("got responce");
   }
 
   final messageInsert = TextEditingController();
@@ -47,19 +34,13 @@ class _LostSomethingState extends State<LostSomething> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Chat bot",
+          "transport Bot",
         ),
+        backgroundColor: kPrimaryColor,
       ),
       body: Container(
         child: Column(
           children: <Widget>[
-            Container(
-              padding: EdgeInsets.only(top: 15, bottom: 10),
-              child: Text(
-                "Today, ${DateFormat("Hm").format(DateTime.now())}",
-                style: TextStyle(fontSize: 20),
-              ),
-            ),
             Flexible(
                 child: ListView.builder(
                     reverse: true,
@@ -67,66 +48,46 @@ class _LostSomethingState extends State<LostSomething> {
                     itemBuilder: (context, index) => chat(
                         messsages[index]["message"].toString(),
                         messsages[index]["data"]))),
-            SizedBox(
-              height: 20,
-            ),
             Divider(
               height: 5.0,
-              color: Colors.greenAccent,
+              color: kPrimaryColor,
             ),
             Container(
-              child: ListTile(
-                leading: IconButton(
-                  icon: Icon(
-                    Icons.camera_alt,
-                    color: Colors.greenAccent,
-                    size: 35,
-                  ),
-                ),
-                title: Container(
-                  height: 35,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(15)),
-                    color: Color.fromRGBO(220, 220, 220, 1),
-                  ),
-                  padding: EdgeInsets.only(left: 15),
-                  child: TextFormField(
+              padding: EdgeInsets.only(left: 15.0, right: 15.0),
+              margin: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Row(
+                children: <Widget>[
+                  Flexible(
+                      child: TextField(
                     controller: messageInsert,
-                    decoration: InputDecoration(
-                      hintText: "Enter a Message...",
-                      hintStyle: TextStyle(color: Colors.black26),
-                      border: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      enabledBorder: InputBorder.none,
-                      errorBorder: InputBorder.none,
-                      disabledBorder: InputBorder.none,
-                    ),
-                    style: TextStyle(fontSize: 16, color: Colors.black),
-                    onChanged: (value) {},
-                  ),
-                ),
-                trailing: IconButton(
-                    icon: Icon(
-                      Icons.send,
-                      size: 30.0,
-                      color: Colors.greenAccent,
-                    ),
-                    onPressed: () {
-                      if (messageInsert.text.isEmpty) {
-                        print("empty message");
-                      } else {
-                        setState(() {
-                          messsages.insert(
-                              0, {"data": 1, "message": messageInsert.text});
-                        });
-                        response(messageInsert.text);
-                        messageInsert.clear();
-                      }
-                      FocusScopeNode currentFocus = FocusScope.of(context);
-                      if (!currentFocus.hasPrimaryFocus) {
-                        currentFocus.unfocus();
-                      }
-                    }),
+                    decoration: InputDecoration.collapsed(
+                        hintText: "Send your message",
+                        hintStyle: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18.0)),
+                  )),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 4.0),
+                    child: IconButton(
+                        icon: Icon(
+                          Icons.send,
+                          size: 30.0,
+                          color: kPrimaryColor,
+                        ),
+                        onPressed: () {
+                          if (messageInsert.text.isEmpty) {
+                            print("empty message");
+                          } else {
+                            setState(() {
+                              messsages.insert(0,
+                                  {"data": 1, "message": messageInsert.text});
+                            });
+                            print(messageInsert.text);
+                            response(messageInsert.text);
+                            messageInsert.clear();
+                          }
+                        }),
+                  )
+                ],
               ),
             ),
             SizedBox(
@@ -141,62 +102,232 @@ class _LostSomethingState extends State<LostSomething> {
   //for better one i have use the bubble package check out the pubspec.yaml
 
   Widget chat(String message, int data) {
-    return Container(
-      padding: EdgeInsets.only(left: 20, right: 20),
-      child: Row(
-        mainAxisAlignment:
-            data == 1 ? MainAxisAlignment.end : MainAxisAlignment.start,
-        children: [
-          data == 0
-              ? Container(
-                  height: 60,
-                  width: 60,
-                  child: CircleAvatar(
-                    backgroundImage: AssetImage("assets/icons/bot_avatar.png"),
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Bubble(
+          radius: Radius.circular(15.0),
+          color: data == 0 ? kPrimaryColor : kPrimaryColor,
+          elevation: 0.0,
+          alignment: data == 0 ? Alignment.topLeft : Alignment.topRight,
+          nip: data == 0 ? BubbleNip.leftBottom : BubbleNip.rightTop,
+          child: Padding(
+            padding: EdgeInsets.all(2.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: AssetImage(data == 0
+                      ? "assets/icons/bot_avatar.png"
+                      : "assets/icons/user_avatar.png"),
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Flexible(
+                    child: Text(
+                  message,
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ))
+              ],
+            ),
+          )),
+    );
+
+    /*List<ChatMessage> messages = [];
+  TextEditingController _inputMessageController = new TextEditingController();
+  Dialogflow dialogflow;
+  AuthGoogle authGoogle;
+  ScrollController _scrollController = new ScrollController(
+    initialScrollOffset: 0.0,
+    keepScrollOffset: true,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration.zero, () async {
+      await initiateDialogFlow();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _scrollToBottom();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        flexibleSpace: SafeArea(
+          child: Container(
+            padding: EdgeInsets.only(right: 16),
+            child: Row(
+              children: <Widget>[
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: Colors.black,
                   ),
-                )
-              : Container(),
-          Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Bubble(
-                radius: Radius.circular(15.0),
-                color: data == 0
-                    ? Color.fromRGBO(23, 157, 139, 1)
-                    : Colors.orangeAccent,
-                elevation: 0.0,
-                child: Padding(
-                  padding: EdgeInsets.all(2.0),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
+                ),
+                SizedBox(
+                  width: 12,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      SizedBox(
-                        width: 10.0,
+                      Text(
+                        "chat-bot",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w600),
                       ),
-                      Flexible(
-                          child: Container(
-                        constraints: BoxConstraints(maxWidth: 200),
-                        child: Text(
-                          message,
-                          style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold),
-                        ),
-                      ))
+                      SizedBox(
+                        height: 6,
+                      ),
+                      Text(
+                        "Online",
+                        style: TextStyle(
+                            color: Colors.grey.shade600, fontSize: 13),
+                      ),
                     ],
                   ),
-                )),
-          ),
-          data == 1
-              ? Container(
-                  height: 60,
-                  width: 60,
-                  child: CircleAvatar(
-                    backgroundColor: kPrimaryColor,
-                    backgroundImage: AssetImage("assets/icons/user_avatar.png"),
-                  ),
                 )
-              : Container(),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            chatSpaceWidget(),
+            Container(
+              height: 1.0,
+              width: double.infinity,
+              color: Colors.blueGrey,
+            ),
+            bottomChatView()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget chatSpaceWidget() {
+    return Flexible(
+      child: SingleChildScrollView(
+        controller: _scrollController,
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            ListView.builder(
+                itemCount: messages.length,
+                shrinkWrap: true,
+                padding: EdgeInsets.only(top: 10, bottom: 10),
+                physics: NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding: EdgeInsets.only(
+                        left: 14, right: 14, top: 10, bottom: 10),
+                    child: Align(
+                      alignment: (messages[index].messageType == "receiver"
+                          ? Alignment.topLeft
+                          : Alignment.topRight),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: (messages[index].messageType == "receiver"
+                              ? Colors.grey.shade200
+                              : Colors.blue[200]),
+                        ),
+                        padding: EdgeInsets.all(16),
+                        child: Text(
+                          messages[index].messageContent,
+                          style: TextStyle(fontSize: 15),
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget bottomChatView() {
+    return Container(
+      padding: EdgeInsets.only(left: 10, bottom: 10, top: 10),
+      height: 60,
+      width: double.infinity,
+      child: Row(
+        children: <Widget>[
+          SizedBox(
+            width: 15,
+          ),
+          Expanded(
+            child: TextField(
+              controller: _inputMessageController,
+              onSubmitted: (String str) {
+                fetchFromDialogFlow(str);
+              },
+              decoration: InputDecoration(
+                  hintText: "Write message...",
+                  hintStyle: TextStyle(color: Colors.black54),
+                  border: InputBorder.none),
+            ),
+          ),
+          SizedBox(
+            width: 15,
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              fetchFromDialogFlow(_inputMessageController.text);
+            },
+            child: Icon(
+              Icons.send,
+              color: Colors.white,
+              size: 18,
+            ),
+            backgroundColor: Colors.blue,
+            elevation: 0,
+          ),
         ],
       ),
     );
+  }
+
+  _scrollToBottom() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (_scrollController.hasClients) {
+        _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
+      }
+    });
+  }
+
+  initiateDialogFlow() async {
+    AuthGoogle authGoogle = await AuthGoogle(
+            fileJson: "assets/services/transport-agent-gpgv-149fd6638fbf.json")
+        .build();
+    dialogflow = Dialogflow(authGoogle: authGoogle, language: Language.english);
+  }
+
+  fetchFromDialogFlow(String input) async {
+    _inputMessageController.clear();
+    setState(() {
+      messages.add(ChatMessage(messageContent: input, messageType: "sender"));
+    });
+
+    AIResponse response = await dialogflow.detectIntent(input);
+    print(response.getMessage());
+    messages.add(ChatMessage(
+        messageContent: response.getMessage(), messageType: "receiver"));
+    setState(() {});*/ /*
+  }*/
   }
 }
