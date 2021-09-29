86d580fcde2ab0920c5f74d013d4648da2705292
Vipooshan-1998 <77396714+Vipooshan-1998@users.noreply.github.com>
2021-09-29 11:52:36 +0530

    updated with 3usecase

---
+++ lib/api/speech_api.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:speech_to_text/speech_to_text.dart';
+
+class SpeechApi {
+  static final _speech = SpeechToText();
+
+  static Future<bool> toggleRecording({
+    @required Function(String text) onResult,
+    @required ValueChanged<bool> onListening,
+  }) async {
+    if (_speech.isListening) {
+      _speech.stop();
+      print('stop');
+      return true;
+    }
+    print('iniatile');
+    final isAvailable = await _speech.initialize(
+      onStatus: (status) => onListening(_speech.isListening),
+      onError: (e) => print('Error: $e'),
+    );
+
+    if (isAvailable) {
+      _speech.listen(onResult: (value) => print(value.recognizedWords));
+    }
+    print(isAvailable);
+    return isAvailable;
+  }
+}
